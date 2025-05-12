f4b21f479ecdb6516d9b274826ea15496a8aa295
Muhammed <faraanshaik19@gmail.com>
2025-05-12 14:01:30 +0530

    react-router commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, DashBoard, Login, NotFound } from "./pages/index";
+import MainLayout from "./layouts/MainLayout";
+import AuthLayout from "./layouts/AuthLayout";
+import PrivateRoute from "./routes/PrivateRoute";
+
 const App = () => {
   return (
     <>
-      <h1>Hello</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+            <Route element={<PrivateRoute />}>
+              <Route path="/dashboard" element={<DashBoard />} />
+            </Route>
+          </Route>
+          <Route element={<AuthLayout />}>
+            <Route path="/login" element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };

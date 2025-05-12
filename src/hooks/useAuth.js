c93ae417a70996e612862ce5431a545bd8a60d5a
Muhammed <faraanshaik19@gmail.com>
2025-05-12 14:41:52 +0530

    useAuth hook commit

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth"; // to persist user login
+import { auth } from "../firebase"; // from firebase file
+
+const useAuth = () => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const user = onAuthStateChanged(auth, (firebaseUser) => {
+      setUser(firebaseUser);
+      setLoading(false);
+    });
+    return () => user();
+  }, []);
+  return { user, loading };
+};
+
+export default useAuth;

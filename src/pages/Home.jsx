f9511e59d0766c577f9c715e0694ba0ab8dc81c9
Muhammed <faraanshaik19@gmail.com>
2025-05-13 22:07:57 +0530

    home commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { user, loading } = useAuth();
+
+  // if (loading) {
+  //   return (
+
+  //   );
+  // }
+
+  return (
+    <section className="min-h-screen bg-base-200 flex flex-col items-center justify-center px-6 text-center">
+      <h1 className="text-4xl md:text-5xl font-bold text-primary mb-4">
+        Welcome to Student Dashboard
+      </h1>
+      <p className="text-base-content text-lg md:w-2/3 mb-6">
+        A modern Firebase-authenticated app built with React, TailwindCSS,
+        DaisyUI, and Lucide Icons. Explore the app features and manage your
+        account with ease.
+      </p>
+
+      {user ? (
+        <Link to="/dashboard" className="btn btn-primary">
+          Go to Dashboard
+        </Link>
+      ) : (
+        <Link to="/login" className="btn btn-accent">
+          Get Started
+        </Link>
+      )}
+    </section>
+  );
 };
 
 export default Home;

76d9d851e246cfee255583e01da865f23ee58df2
Muhammed <faraanshaik19@gmail.com>
2025-05-14 20:37:30 +0530

    login pagr updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+    if (!email.trim() && !password.trim()) {
+      toast.error("All the fields are required");
+      return;
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input type="submit" value="button" className="btn btn-primary" />
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Login;
